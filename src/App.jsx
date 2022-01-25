687635c851cecfe742816f6e2cfe1dc77b100571
Your Name <you@example.com>
2022-01-25 01:48:37 -0600

    React app using Lyric.ovh api

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, Fragment, useEffect } from 'react'
+import axios from 'axios';
+
+import Formulario from './components/Formulario'
+
+import Cancion from './components/Cancion';
+
+import Info from './components/Info';
+
+function App() {
+
+  const [busquedaLetra, setBusquedaLetra] = useState({});
+  const [letra, setLetra] = useState('');
+  const [info, setInfo] = useState('');
+
+  useEffect(() => {
+
+    if(Object.keys(busquedaLetra).length === 0) return;
+
+    const consultarApiLetra = async() => {
+
+      const {artista, cancion} = busquedaLetra
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `theaudiodb.com/api/v1/json/2/search.php?s=${artista}`; //YA no sirve de modo gratuito :(
+
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        /* axios(url2) */
+      ])
+
+      setLetra(letra.data.lyrics);
+      setInfo(informacion)
+
+    }
+
+    consultarApiLetra();
+
+  }, [busquedaLetra]);
+
+  return (
+    <Fragment>
+      <Formulario 
+        setBusquedaLetra={setBusquedaLetra}
+      />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info}/>
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra}/>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
